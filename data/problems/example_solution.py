76278301459b3510b06bcc889dcd57b9eeffd9ba
Matthew Merrill <MattMerr47@gmail.com>
2018-04-16 23:03:27 -0700

    example cases, wacky weights

---
+++ data/problems/example_solution.py
@@ -0,0 +1,13 @@
+
+def sumRange(n):
+    if n == 0:
+        return 0
+    #elif n < 0:
+    #    return -sumRange(-n) + 1
+    else:
+        return n * (n + 1) // 2
+
+n = int(input())
+while n != 0:
+    print("N = %d\tSum = %d" % (n, sumRange(n)))
+    n = int(input())
